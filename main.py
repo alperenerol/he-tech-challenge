c8b0b492255f6abe11d98d5583db53f7ea2715b8
Alperen Erol <aerol2011@gmail.com>
2024-08-06 10:22:40 +0100

    modified main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 from datetime import datetime
 import configparser
 
-from api.fetch_data import fetch_auction_results, filter_results, detect_fields
+from api.fetch_data import fetch_auction_results, detect_fields
 from db.dynamic_schema import create_dynamic_table, save_results
 
 # Configure logging
@@ -12,15 +12,13 @@ logging.basicConfig(filename='errors.log', level=logging.INFO,
 # Load configuration
 config = configparser.ConfigParser()
 config.read('config.ini')
+participant_name = config['api'].get('participant_name')
+current_date = datetime.now().date()
 
 def main():
-    current_date = datetime.now().date()
-    participant_name = config['api'].get('participant_name')
-
     try:
         results = fetch_auction_results(participant_name)
         fields = detect_fields(results)
-        #current_date_results = filter_results(results)
         auction_results_table = create_dynamic_table(fields)
         save_results(results, auction_results_table)
         
